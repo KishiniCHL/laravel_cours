cb5a450a0648d14daa8d52468a06967b220cfba1
Hoai-lan DINH VU <christiane.dinhvu@gmail.com>
2024-07-09 09:45:08 +0200

    correct files

--- resources/js/Pages/Track/Index.vue
+++ resources/js/Pages/Track/Index.vue
@@ -1,35 +1,33 @@
 <template>
-    <MusicLayout>
-        <template #content>   
-            <div>
-                liste de mes tracks 
-
-                {{ tracks }}
-
-                <ul>
-                    <li v-for="track in tracks"
-                    :key="track.uuid">
-                    {{ track.title }} - {{ track.artist }} - {{ track.album }} - {{ track.genre }} - {{ track.year }} - {{ track.duration }} - {{ track.size }} - {{ track.bitrate }} - {{ track.path }} - {{ track.created_at }} - {{ track.updated_at }}
-                    </li>
-                </ul>
-            </div>
-        </template>
-</MusicLayout>
+  <MusicLayout>
+    <template #title>
+      Liste de mes tracks
+    </template>
+    <template #action>
+      Ajouter une musique
+    </template>
+    <template #content>
+      <div>
+        <ul>
+          <li v-for="track in tracks" :key="track.uuid">
+            {{ track.title }} ({{ track.artist }})
+          </li>
+        </ul>
+      </div>
+    </template>
+  </MusicLayout>
 </template>
 
 <script>
-
-import MusicLayout from '@/Layouts/MusicLayout.vue'; 
+import MusicLayout from '@/Layouts/MusicLayout.vue';
 
 export default {
-    name: 'Index',
-    components: {
-        MusicLayout,
-    },
-    props: {
-        tracks: Array,
-    },
-    
+  name: 'Index',
+  components: {
+    MusicLayout,
+  },
+  props: {
+    tracks: Array,
+  },
 }
-
 </script>
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,40 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TrackController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
-// Route::get('/tracks', [TrackController::class, 'index'])->name('tracks.index');
-// Route::get('/tracks/create', [TrackController::class, 'create'])->name('tracks.create');
-// Route::post('/tracks', [TrackController::class, 'store'])->name('tracks.store');
-// Route::get('/tracks/{track}', [TrackController::class, 'show'])->name('tracks.show');
-// Route::get('/tracks/{track}/edit', [TrackController::class, 'edit'])->name('tracks.edit');
-// Route::put('/tracks/{track}', [TrackController::class, 'update'])->name('tracks.update');
-// Route::patch('/tracks/{track}', [TrackController::class, 'update']);
-// Route::delete('/tracks/{track}', [TrackController::class, 'destroy'])->name('tracks.destroy');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
 
-Route::name('tracks.')->prefix('tracks')->controller(TrackController::class)->group(function () {
-    // Route::get('/', [TrackController::class,'index'])->name('index');
+Route::get('/test', [HomeController::class, 'test'])->name('test');
+
+Route::controller(TrackController::class)->prefix('tracks')->name('tracks.')->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/create', 'create')->name('create');
     Route::post('/', 'store')->name('store');
     Route::get('/{track}', 'show')->name('show');
     Route::get('/{track}/edit', 'edit')->name('edit');
     Route::put('/{track}', 'update')->name('update');
-    Route::patch('/{track}', 'update');
     Route::delete('/{track}', 'destroy')->name('destroy');
 });
 
-// creer directement le crud
-// Route::ressource('tracks', TrackController::class);
+// Route::resource('tracks', TrackController::class)->except(['show']);
